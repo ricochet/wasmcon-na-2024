4dfeda1dc828c95690bdcb66e9c327df668c7260
Bailey Hayes <behayes2@gmail.com>
2024-11-10 23:21:28 -0700

    fix(rust): add part 2 for dog-fetcher

--- rust/dog-fetcher/src/lib.rs
+++ rust/dog-fetcher/src/lib.rs
@@ -24,19 +24,25 @@ impl Server for DogFetcher {
         )
         .map_err(|_| internal_error("failed to deserialize dog API response"))?;
 
-        // Form the object key using the last 3 segments of the URL
-        let dog_image_name = dog_response
-            .message
-            .split('/')
-            .skip(4)
-            .collect::<Vec<_>>()
-            .join("-");
-        let dog_picture = http::request(&dog_response.message)?;
-        objstore::write_object(dog_picture, &CONTAINER_NAME.to_string(), &dog_image_name)?;
-
+        // remove for PART 2:
         Ok(Response::new(
-            json!({"dog_file": format!("/tmp/{CONTAINER_NAME}/{dog_image_name}")}).to_string(),
+            json!({"dog_file": &dog_response.message}).to_string(),
         ))
+
+        // PART 2: uncomment
+        // // Form the object key using the last 3 segments of the URL
+        // let dog_image_name = dog_response
+        //     .message
+        //     .split('/')
+        //     .skip(4)
+        //     .collect::<Vec<_>>()
+        //     .join("-");
+        // let dog_picture = http::request(&dog_response.message)?;
+        // objstore::write_object(dog_picture, &CONTAINER_NAME.to_string(), &dog_image_name)?;
+
+        // Ok(Response::new(
+        //     json!({"dog_file": format!("/tmp/{CONTAINER_NAME}/{dog_image_name}")}).to_string(),
+        // ))
     }
 }
 
--- rust/dog-fetcher/src/objstore.rs
+++ rust/dog-fetcher/src/objstore.rs
@@ -1,55 +1,56 @@
 //! Object storage related functionality and helper methods
 
-use wasmcloud_component::trace;
-use wasmcloud_component::wasi::blobstore::blobstore::{
-    container_exists, create_container, get_container,
-};
-use wasmcloud_component::wasi::blobstore::container::Container;
-use wasmcloud_component::wasi::blobstore::types::OutgoingValue;
-use wasmcloud_component::wasi::http::outgoing_handler::ErrorCode;
-use wasmcloud_component::wasi::http::types::IncomingBody;
-use wasmcloud_component::wasi::io::streams::StreamError;
+// PART 2:
+// use wasmcloud_component::trace;
+// use wasmcloud_component::wasi::blobstore::blobstore::{
+//     container_exists, create_container, get_container,
+// };
+// use wasmcloud_component::wasi::blobstore::container::Container;
+// use wasmcloud_component::wasi::blobstore::types::OutgoingValue;
+// use wasmcloud_component::wasi::http::outgoing_handler::ErrorCode;
+// use wasmcloud_component::wasi::http::types::IncomingBody;
+// use wasmcloud_component::wasi::io::streams::StreamError;
 
-use crate::internal_error;
+// use crate::internal_error;
 
-/// A helper that will automatically create a container if it doesn't exist and returns an owned copy of the name for immediate use
-pub(crate) fn ensure_container(name: &String) -> Result<Container, ErrorCode> {
-    if container_exists(name).map_err(internal_error)? {
-        return get_container(name).map_err(internal_error);
-    }
-    create_container(name).map_err(internal_error)
-}
+// /// A helper that will automatically create a container if it doesn't exist and returns an owned copy of the name for immediate use
+// pub(crate) fn ensure_container(name: &String) -> Result<Container, ErrorCode> {
+//     if container_exists(name).map_err(internal_error)? {
+//         return get_container(name).map_err(internal_error);
+//     }
+//     create_container(name).map_err(internal_error)
+// }
 
-/// Stream a binary blob from an HTTP [`IncomingBody`] to object storage
-pub(crate) fn write_object(
-    object_body: IncomingBody,
-    bucket: &String,
-    key: &String,
-) -> Result<(), ErrorCode> {
-    let container = ensure_container(&String::from(bucket))?;
+// /// Stream a binary blob from an HTTP [`IncomingBody`] to object storage
+// pub(crate) fn write_object(
+//     object_body: IncomingBody,
+//     bucket: &String,
+//     key: &String,
+// ) -> Result<(), ErrorCode> {
+//     let container = ensure_container(&String::from(bucket))?;
 
-    let data = OutgoingValue::new_outgoing_value();
-    let data_body = data
-        .outgoing_value_write_body()
-        .map_err(|()| internal_error("failed to get data output stream"))?;
-    container.write_data(key, &data).map_err(internal_error)?;
-    let object_stream = object_body
-        .stream()
-        .map_err(|()| internal_error("failed to stream object"))?;
-    loop {
-        match data_body.blocking_splice(&object_stream, u64::MAX) {
-            Ok(0) | Err(StreamError::Closed) => break,
-            Ok(n) => {
-                trace!("wrote {n} bytes to object storage");
-                continue;
-            }
-            Err(e) => return Err(internal_error(e)),
-        }
-    }
-    drop(data_body);
-    drop(object_stream);
-    OutgoingValue::finish(data).map_err(internal_error)?;
-    let _trailers = IncomingBody::finish(object_body);
+//     let data = OutgoingValue::new_outgoing_value();
+//     let data_body = data
+//         .outgoing_value_write_body()
+//         .map_err(|()| internal_error("failed to get data output stream"))?;
+//     container.write_data(key, &data).map_err(internal_error)?;
+//     let object_stream = object_body
+//         .stream()
+//         .map_err(|()| internal_error("failed to stream object"))?;
+//     loop {
+//         match data_body.blocking_splice(&object_stream, u64::MAX) {
+//             Ok(0) | Err(StreamError::Closed) => break,
+//             Ok(n) => {
+//                 trace!("wrote {n} bytes to object storage");
+//                 continue;
+//             }
+//             Err(e) => return Err(internal_error(e)),
+//         }
+//     }
+//     drop(data_body);
+//     drop(object_stream);
+//     OutgoingValue::finish(data).map_err(internal_error)?;
+//     let _trailers = IncomingBody::finish(object_body);
 
-    Ok(())
-}
+//     Ok(())
+// }
