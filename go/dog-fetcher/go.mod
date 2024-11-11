4170fa510cd9965fdb51391213abe83dcd5bf8c2
Joonas Bergius <joonas@cosmonic.com>
2024-11-10 22:07:57 -0700

    feat(dog-fetcher-go): Update

--- go/dog-fetcher/go.mod
+++ go/dog-fetcher/go.mod
@@ -1,4 +1,4 @@
-module github.com/wasmcloud/wasmcloud/examples/golang/components/http-hello-world
+module github.com/ricochet/wasmcon-na-2024/go/dog-fetcher
 
 go 1.23.0
 
@@ -14,11 +14,14 @@ require (
 	github.com/klauspost/compress v1.17.11 // indirect
 	github.com/opencontainers/go-digest v1.0.0 // indirect
 	github.com/regclient/regclient v0.7.2 // indirect
+	github.com/samber/lo v1.47.0 // indirect
+	github.com/samber/slog-common v0.17.1 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
 	github.com/ulikunitz/xz v0.5.12 // indirect
 	github.com/urfave/cli/v3 v3.0.0-alpha9.2 // indirect
 	golang.org/x/mod v0.21.0 // indirect
 	golang.org/x/sync v0.8.0 // indirect
 	golang.org/x/sys v0.26.0 // indirect
+	golang.org/x/text v0.18.0 // indirect
 	golang.org/x/tools v0.26.0 // indirect
 )
---
+++ go/dog-fetcher/objstore.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"errors"
+	"io"
+
+	"github.com/bytecodealliance/wasm-tools-go/cm"
+	"github.com/ricochet/wasmcon-na-2024/go/dog-fetcher/gen/wasi/blobstore/v0.2.0-draft/blobstore"
+	"github.com/ricochet/wasmcon-na-2024/go/dog-fetcher/gen/wasi/blobstore/v0.2.0-draft/types"
+)
+
+func writeObject(reader io.ReadCloser, containerName string, filename string) error {
+	// Get the wasi:blobstore container to store the image in
+	container, err := ensureContainer(containerName)
+	if err != nil {
+		return err
+	}
+
+	// Setup an outgoing request to store
+	outgoingValue := types.OutgoingValueNewOutgoingValue()
+	outgoingStreamRes := outgoingValue.OutgoingValueWriteBody()
+	if outgoingStreamRes.IsErr() {
+		return errors.New("failed to open a new stream for storing the object")
+	}
+	outgoingStream := outgoingStreamRes.OK()
+	defer outgoingStream.ResourceDrop()
+
+	// Set up the write to blobstore
+	write := container.WriteData(types.ObjectName(filename), outgoingValue)
+	if write.IsErr() {
+		return errors.New("failed to start writing data to blob store")
+	}
+
+	// Read 4096 bytes at a time and write it to the blobstore stream
+	buf := make([]byte, 4096)
+	for {
+		written, err := reader.Read(buf)
+		if err != nil {
+			return errors.New("failed to flush bytes to blob store")
+		}
+		if written == 0 {
+			break
+		}
+		writeRes := outgoingStream.BlockingWriteAndFlush(cm.ToList(buf))
+		if writeRes.IsErr() {
+			return errors.New("failed to flush bytes to blob store")
+		}
+		if written < 4096 {
+			break
+		}
+	}
+	return nil
+}
+
+func ensureContainer(name string) (*blobstore.Container, error) {
+	containerName := types.ContainerName(name)
+	exists := blobstore.ContainerExists(containerName)
+	if exists.IsOK() {
+		containerReq := blobstore.GetContainer(containerName)
+		if containerReq.IsErr() {
+			return nil, errors.New("container does not exist")
+		}
+		return containerReq.OK(), nil
+	}
+
+	containerReq := blobstore.CreateContainer(containerName)
+	if containerReq.IsErr() {
+		return nil, errors.New("could not create a new container")
+	}
+	return containerReq.OK(), nil
+}
